1071de730d5e175e69ec75d7f37e719df79301e3
asantanadurango <asantanadurango@gmail.com>
2022-02-15 16:44:56 -0500

    CheckBox básico

--- app/src/main/java/com/example/miprimerapp/MainActivity.java
+++ app/src/main/java/com/example/miprimerapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -13,64 +14,39 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private EditText n1, n2;
-    private RadioButton rb1, rb2, rb3, rb4;
+    private CheckBox chb1, chb2;
     private TextView resultado;
     private String defaultText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("TAG", String.valueOf(n1));
         setContentView(R.layout.activity_main);
         n1 = (EditText)findViewById(R.id.n1);
         n2 = (EditText)findViewById(R.id.n2);
-        rb1 = (RadioButton)findViewById(R.id.rb1);
-        rb2 = (RadioButton)findViewById(R.id.rb2);
-        rb3 = (RadioButton)findViewById(R.id.rb3);
-        rb4 = (RadioButton)findViewById(R.id.rb4);
+        chb1 = (CheckBox)findViewById(R.id.chb1);
+        chb2 = (CheckBox)findViewById(R.id.chb2);
         resultado = (TextView) findViewById(R.id.resultado);
         defaultText = resultado.getText().toString();
+
     }
 
     public void click(View view){
-        int n1_value = Integer.parseInt(String.valueOf(n1.getText()));
-        int n2_value = Integer.parseInt(String.valueOf(n2.getText()));
-        int resultado_value;
-        String n1_str = String.valueOf(n1_value);
-        String n2_str = String.valueOf(n2_value);
+        String n1_str = "Mi nombre es: " + n1.getText() + ".";
+        String n2_str = "Tengo: " + n2.getText() + " años.";
         String resultado_str;
-
-        if(rb1.isChecked()){
-            resultado_value = n1_value + n2_value;
-            resultado_str = String.valueOf(resultado_value);
-            resultado.setText(defaultText + ": " + n1_str + " + " + n2_str + " = " + resultado_str );
-            return;
+        if(chb1.isChecked()){
+            resultado.setText(n1_str);
         }
-        if(rb2.isChecked()){
-            resultado_value = n1_value - n2_value;
-            resultado_str = String.valueOf(resultado_value);
-            resultado.setText(defaultText + ": " + n1_str + " - " + n2_str + " = " + resultado_str );
-            return;
+        if(chb2.isChecked()){
+            resultado.setText(n2_str);
         }
 
-        if(rb3.isChecked()){
-            resultado_value = n1_value * n2_value;
-            resultado_str = String.valueOf(resultado_value);
-            resultado.setText(defaultText + ": " + n1_str + " x " + n2_str + " = " + resultado_str );
-            return;
-        }
-
-        if(rb4.isChecked()){
-            if(n2_value == 0){
-                Toast.makeText(this, "No puedes dividir por 0", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            resultado_value = n1_value / n2_value;
-            resultado_str = String.valueOf(resultado_value);
-            resultado.setText(defaultText + ": " + n1_str + " / " + n2_str + " = " + resultado_str );
-            return;
+        if(chb1.isChecked() && chb2.isChecked()){
+            resultado.setText(n1_str +"\n" + n2_str);
         }
 
     }
+
 
 
 
