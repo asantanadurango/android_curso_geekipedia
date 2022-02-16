93415e31b17d82ff1aa4c885b38441e3c25448cd
asantanadurango <asantanadurango@gmail.com>
2022-02-15 21:19:06 -0500

    Rellenar spinner - switch - values->strind¿sgs

--- app/src/main/java/com/example/miprimerapp/MainActivity.java
+++ app/src/main/java/com/example/miprimerapp/MainActivity.java
@@ -2,19 +2,20 @@ package com.example.miprimerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.CheckBox;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText n1, n2;
-    private CheckBox chb1, chb2;
+    private Spinner spinner;
     private TextView resultado;
     private String defaultText;
     @Override
@@ -23,32 +24,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         n1 = (EditText)findViewById(R.id.n1);
         n2 = (EditText)findViewById(R.id.n2);
-        chb1 = (CheckBox)findViewById(R.id.chb1);
-        chb2 = (CheckBox)findViewById(R.id.chb2);
+        spinner = (Spinner)findViewById(R.id.spinner);
         resultado = (TextView) findViewById(R.id.resultado);
         defaultText = resultado.getText().toString();
 
+        // Dos maneras de rellenar un Spinner.
+        Resources res = getResources();
+        String[] items =  res.getStringArray(R.array.items);
+        String[] opciones = {"Sumar", "Restar", "Multiplicar", "Dividir"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        spinner.setAdapter(arrayAdapter);
     }
 
     public void click(View view){
+        int n1_value = Integer.parseInt(String.valueOf(n1.getText()));
+        int n2_value = Integer.parseInt(String.valueOf(n2.getText()));
+        int resultado_value;
         String n1_str = "Mi nombre es: " + n1.getText() + ".";
         String n2_str = "Tengo: " + n2.getText() + " años.";
         String resultado_str;
-        if(chb1.isChecked()){
-            resultado.setText(n1_str);
-        }
-        if(chb2.isChecked()){
-            resultado.setText(n2_str);
-        }
+        String opcionSeleccionada = spinner.getSelectedItem().toString();
+        String id = String.valueOf(spinner.getSelectedItemId());
+        String posicion = String.valueOf(spinner.getSelectedItemPosition());
+        Toast.makeText(this, "Id: " +id+" posición: " +posicion, Toast.LENGTH_SHORT).show();
 
-        if(chb1.isChecked() && chb2.isChecked()){
-            resultado.setText(n1_str +"\n" + n2_str);
-        }
+        switch (opcionSeleccionada){
+            case "Sumar":
+                resultado_value = n1_value + n2_value;
+                resultado_str = String.valueOf(resultado_value);
+                resultado.setText(resultado_str);
+            break;
+            case "Restar":
+                resultado_value = n1_value - n2_value;
+                resultado_str = String.valueOf(resultado_value);
+                resultado.setText(resultado_str);
+                break;
 
+            case "Multiplicar":
+                resultado_value = n1_value * n2_value;
+                resultado_str = String.valueOf(resultado_value);
+                resultado.setText(resultado_str);
+                break;
+            case "Dividir":
+                if(n2_value == 0){
+                    Toast.makeText(this, "No se puede dividir por cero", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                resultado_value = n1_value / n2_value;
+                resultado_str = String.valueOf(resultado_value);
+                resultado.setText(resultado_str);
+                break;
+            default: resultado.setText("0");
+        }
     }
-
-
-
-
-
 }
